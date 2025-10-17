a75994a79ea95f7a8117776b81dd74725b7cedcd
STUTZ Clément <contact@clementstutz.fr>
2025-10-17 09:36:18 +0200

    Add tests in Intersection_Test.cpp

--- tests/Intersection_Test.cpp
+++ tests/Intersection_Test.cpp
@@ -1,13 +1,13 @@
 #include "pch.h"
-#include "Point.h"
-#include "Vector.h"
-#include "Ray.h"
 #include "Sphere.h"
 
 TEST(IntersectionsTests, constructor) {
     Sphere sphere;
     Intersection intersection(sphere, 1.2);
 
+    EXPECT_EQ(intersection == intersection, true);
+    EXPECT_EQ(intersection.IsEmpty(), false);
     EXPECT_EQ(intersection.getObj(), sphere);
+    EXPECT_EQ(intersection.getObjPtr(), &sphere);
     EXPECT_FLOAT_EQ(intersection.getLength(), 1.2);
 }
